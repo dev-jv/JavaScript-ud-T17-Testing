d48a43f17bdbcf9848ad15dc00b977a12c085151
dev-jv <dev.javierleon@gmail.com>
2020-11-15 22:56:52 -0500

    one

---
+++ js/01-app.js
@@ -0,0 +1,28 @@
+// ------------------- <> Mini Framework - Testing (1)
+
+
+function suma(a, b) {
+    return a + b;
+}
+
+function restar(a,b) {
+    return a - b;
+}
+
+let resultado = suma(2, 2); 
+let esperado = 3;
+
+if(resultado !== esperado) {
+    console.error(`El ${resultado} es diferente a lo esperado; la prueba no pasó`);
+} else {
+    console.log('La prueba paso correctamente');
+}
+
+resultado = restar(10, 5);
+esperado = 6;
+
+if(resultado !== esperado) {
+    console.error(`El ${resultado} es diferente a lo esperado; la prueba no pasó`);
+} else {
+    console.log('La prueba paso correctamente');
+}
